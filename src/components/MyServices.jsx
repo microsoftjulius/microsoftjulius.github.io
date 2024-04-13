eb125b933158b742fdd8743ead6bb81293c5eadd
microsoftjulius <julisema4@gmail.com>
2024-04-13 14:52:51 +0300

    its working well offline

--- src/components/MyServices.jsx
+++ src/components/MyServices.jsx
@@ -8,7 +8,7 @@ export default function MyServices(){
                     <div className="col-md-6 col-lg-3">
                         <div className="service-card">
                             <div className="body">
-                                <img src="/src/assets/imgs/react.png" alt="image" className="icon" style={{ width: '40px' }}/>
+                                <img src="https://private-user-images.githubusercontent.com/32195353/322202861-2c223487-19a8-4ced-b94f-01d49a9d3d9a.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMwMDg4ODksIm5iZiI6MTcxMzAwODU4OSwicGF0aCI6Ii8zMjE5NTM1My8zMjIyMDI4NjEtMmMyMjM0ODctMTlhOC00Y2VkLWI5NGYtMDFkNDlhOWQzZDlhLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDEzVDExNDMwOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWU0NzNiZTQxYmQ5ZmE5MmZjOWY1YTEzZThkYTNmNzI3MDM5OGVmYTk0ZTFhNjhhZWIzNGU3MDBmNWRmMWE5OWEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.Lwa1BT9eyGW74Lmie6Nd7ARo9Vcg0C_pvKGSN-0xWTY" alt="image" className="icon" style={{ width: '40px' }}/>
                                 <h6 className="title">Reactjs</h6>
                                 <p className="subtitle">Building interactive and responsive web applications with Reactjs.</p>
                             </div>
@@ -17,7 +17,7 @@ export default function MyServices(){
                     <div className="col-md-6 col-lg-3">
                         <div className="service-card">
                             <div className="body">
-                                <img src="/src/assets/imgs/flask.png" alt="image" className="icon" style={{ width: '45px' }}/>
+                                <img src="https://private-user-images.githubusercontent.com/32195353/322203106-d18d7916-d3c1-4143-909c-b2ac8d9cdf97.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMwMDg4ODksIm5iZiI6MTcxMzAwODU4OSwicGF0aCI6Ii8zMjE5NTM1My8zMjIyMDMxMDYtZDE4ZDc5MTYtZDNjMS00MTQzLTkwOWMtYjJhYzhkOWNkZjk3LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDEzVDExNDMwOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWFmNTAwM2Y3YjEyNzRmY2M5MDg5MWIxNjMwYzY4Njg1N2Q0ZWEyM2IyNGQ0N2UyMmM2YjAxOTJkZjI2OWNmMjYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.2hqBgICH7uCsNMbaM-sZ_Y-sNBHjqQu13bt-HIiv2KI" alt="image" className="icon" style={{ width: '45px' }}/>
                                 <h6 className="title">Flask</h6>
                                 <p className="subtitle">Developing lightweight and scalable web applications using Flask.</p>
                             </div>
@@ -26,7 +26,7 @@ export default function MyServices(){
                     <div className="col-md-6 col-lg-3">
                         <div className="service-card">
                             <div className="body">
-                                <img src="/src/assets/imgs/laravel.png" alt="image" className="icon" style={{ width: '70px' }}/>
+                                <img src="https://private-user-images.githubusercontent.com/32195353/322203323-2b5ab3da-8457-4d22-b8c9-a66f7205abfb.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMwMDg4ODYsIm5iZiI6MTcxMzAwODU4NiwicGF0aCI6Ii8zMjE5NTM1My8zMjIyMDMzMjMtMmI1YWIzZGEtODQ1Ny00ZDIyLWI4YzktYTY2ZjcyMDVhYmZiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDEzVDExNDMwNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTBkZjA4Y2MxYTJiNTEzYjMyNzhkZTRlMTFiOGY2MWVjMTMxY2UwYTU0Y2Q5MGJlODdkYjkxNmM5MjU0OGY0NWImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.CwXMM4yBMpFf-e2vn0qP6ChZdszZ4_aEjYn34ebU4kA" alt="image" className="icon" style={{ width: '70px' }}/>
                                 <h6 className="title">Laravel</h6>
                                 <p className="subtitle">Creating robust and feature-rich web applications with Laravel.</p>
                             </div>
@@ -35,7 +35,7 @@ export default function MyServices(){
                     <div className="col-md-6 col-lg-3">
                         <div className="service-card">
                             <div className="body">
-                                <img src="/src/assets/imgs/flutter.png" alt="image" className="icon" style={{ width: '35px' }}/>
+                                <img src="https://private-user-images.githubusercontent.com/32195353/322202944-dff1130d-a9c2-4cc3-b573-2ffacc661dbb.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMwMDg3MTIsIm5iZiI6MTcxMzAwODQxMiwicGF0aCI6Ii8zMjE5NTM1My8zMjIyMDI5NDQtZGZmMTEzMGQtYTljMi00Y2MzLWI1NzMtMmZmYWNjNjYxZGJiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDEzVDExNDAxMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ4MzRmNTE5MTE5OGUyMWNkZDZjYTBhOGQ1Y2FhMjU2MGY0MjAzZDI2NjE3ODM5M2FmOWIzZTMwOWQxOTg3MTYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.MeelCw44RqtRndr86gtUqemJizdwc_El8XEv7M1QZZ4" alt="image" className="icon" style={{ width: '35px' }}/>
                                 <h6 className="title">Flutter</h6>
                                 <p className="subtitle">Building cross-platform mobile applications using Flutter.</p>
                             </div>
@@ -46,7 +46,7 @@ export default function MyServices(){
                     <div className="col-md-6 col-lg-3">
                         <div className="service-card">
                             <div className="body">
-                                <img src="/src/assets/imgs/django.png" alt="image" className="icon" style={{ width: '40px' }}/>
+                                <img src="https://private-user-images.githubusercontent.com/32195353/322202917-5571a1eb-3914-4228-9dee-2e4e145c5035.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMwMDg3MTIsIm5iZiI6MTcxMzAwODQxMiwicGF0aCI6Ii8zMjE5NTM1My8zMjIyMDI5MTctNTU3MWExZWItMzkxNC00MjI4LTlkZWUtMmU0ZTE0NWM1MDM1LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDEzVDExNDAxMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTFmYWM2MTViNjRkZjNhZDlhZTkxM2MxOTQ3N2Y3YWNkYWY4OTk3NjA3NzcwNGRlMWFiMmRmMzcyZjNjYjJhM2YmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.LQkvEZvxOX2ErzKSbR0Fel-edJEPjlNUa8yg2jD94HQ" alt="image" className="icon" style={{ width: '40px' }}/>
                                 <h6 className="title">Django</h6>
                                 <p className="subtitle">Developing high-performance web applications with Django.</p>
                             </div>
@@ -55,7 +55,7 @@ export default function MyServices(){
                     <div className="col-md-6 col-lg-3">
                         <div className="service-card">
                             <div className="body">
-                                <img src="/src/assets/imgs/docker.png" alt="image" className="icon" style={{ width: '50px' }}/>
+                                <img src="https://private-user-images.githubusercontent.com/32195353/322202931-8921acef-20e8-4a17-84aa-f721da4e364d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMwMDg3MTIsIm5iZiI6MTcxMzAwODQxMiwicGF0aCI6Ii8zMjE5NTM1My8zMjIyMDI5MzEtODkyMWFjZWYtMjBlOC00YTE3LTg0YWEtZjcyMWRhNGUzNjRkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDEzVDExNDAxMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTNlMGMwMzY3OTE3NjJlMzRmMjkwODUxODQ4ZmNkNDA3YjVhMGVmZmRhZjViMDFhMDJmMzZlMjVlMmZhODQ2ZGQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.KudcL7uw3tA63ZRfF9WJZZk8OprFOOXVK9dgHMm0ENg" alt="image" className="icon" style={{ width: '50px' }}/>
                                 <h6 className="title">Docker</h6>
                                 <p className="subtitle">Containerizing and deploying applications with Docker.</p>
                             </div>
@@ -64,7 +64,7 @@ export default function MyServices(){
                     <div className="col-md-6 col-lg-3">
                         <div className="service-card">
                             <div className="body">
-                                <img src="/src/assets/imgs/postgres.png" alt="image" className="icon" style={{ width: '50px' }}/>
+                                <img src="https://private-user-images.githubusercontent.com/32195353/322202553-df9fd2ca-9026-4ae8-8299-809f0ed05e4d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMwMDg3MTIsIm5iZiI6MTcxMzAwODQxMiwicGF0aCI6Ii8zMjE5NTM1My8zMjIyMDI1NTMtZGY5ZmQyY2EtOTAyNi00YWU4LTgyOTktODA5ZjBlZDA1ZTRkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDEzVDExNDAxMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTQ1ZDRiNjY4OWI1YzI3MDYxOGRmZmM1YzczYzEyMmNhNDAwMmE5NDFlMTFjYzQwZGI1YTk1MGUwNTMwMmM2NmQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.p57qeuApPyHVrCos-TFa1kFC9_5gDenu8q9Amb1EEVs" alt="image" className="icon" style={{ width: '50px' }}/>
                                 <h6 className="title">Postgres</h6>
                                 <p className="subtitle">Managing relational databases efficiently with PostgreSQL.</p>
                             </div>
@@ -73,7 +73,7 @@ export default function MyServices(){
                     <div className="col-md-6 col-lg-3">
                         <div className="service-card">
                             <div className="body">
-                                <img src="/src/assets/imgs/mongodb.png" alt="image" className="icon" style={{ width: '50px' }}/>
+                                <img src="https://private-user-images.githubusercontent.com/32195353/322202304-530a4bc0-b5c8-4a21-b00c-d6def469e90f.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTMwMDg3MTIsIm5iZiI6MTcxMzAwODQxMiwicGF0aCI6Ii8zMjE5NTM1My8zMjIyMDIzMDQtNTMwYTRiYzAtYjVjOC00YTIxLWIwMGMtZDZkZWY0NjllOTBmLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MTMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDEzVDExNDAxMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWFkNDVkMmJlYzQwNjMwMGMyODY2OWY4Nzk5NDFiZWUyYzQ1ZGJlZDE5NWFmOGRiZmYwMWNkZDU4NjM2NzZkYjgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.yOxSZMCHC1J3fFCQ5k8sF4OBFG8W9hV38Q2SiMBx4_E" alt="image" className="icon" style={{ width: '50px' }}/>
                                 <h6 className="title">MongoDB</h6>
                                 <p className="subtitle">Storing and retrieving data seamlessly with MongoDB.</p>
                             </div>
