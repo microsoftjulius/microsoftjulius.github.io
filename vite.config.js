bc331970c189960975f3c8d7e2783178f80bf531
microsoftjulius <julisema4@gmail.com>
2024-04-13 13:51:09 +0300

    its working well offline

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/microsoftjulius.github.io/",
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['/microsoftjulius.github.io/src/assets/vendors/jquery/jquery-3.4.1.js',
+      '/microsoftjulius.github.io/src/assets/vendors/bootstrap/bootstrap.bundle.js',
+      '/microsoftjulius.github.io/src/assets/css/meyawo.css',
+      '/microsoftjulius.github.io/src/assets/vendors/bootstrap/bootstrap.affix.js',
+      '/microsoftjulius.github.io/src/assets/js/meyawo.js'
+    ]
+    }
+  }
+});
