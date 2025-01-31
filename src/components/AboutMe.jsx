e83054f52854899ec0453bdb511bebae5fd2357a
microsoftjulius <julisema4@gmail.com>
2025-01-31 23:27:50 +0300

    added Bar-travel and Turing work

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -12,11 +12,16 @@ export default function AboutMe(){
                     <p className="section-subtitle">Who Am I ?</p>
                     <h2 className="section-title mb-3">About Me</h2>
                     <p style={{ textAlign: "justify" }}>
-                        I am a Versatile full-stack developer skilled in Python (Flask, Django), PHP (Laravel), JavaScript (React), Flutter, 
-                        and Blockchain (Web3, Solidity). Proficient in database management (PostgreSQL, MySQL, MongoDB) and adept at crafting responsive web apps. 
-                        Seeking new challenges in software development.
+                    I am a dynamic Software Developer with more than 7 years of diverse experience, specialising in 
+                    innovative application development and AI training. Expertise in problem-solving and 
+                    enhancing system performance through strategic integration of technologies such as Python, 
+                    Flask, and Laravel. Proven ability to lead projects that improve user engagement and streamline 
+                    processes, demonstrating a strong commitment to delivering impactful solutions. Skilled in utilizing 
+                    advanced programming languages and frameworks, as well as collaborating effectively in team environments 
+                    to achieve high-quality outcomes. Passionate about leveraging technology to drive efficiency and make a meaningful 
+                    difference in the community.
                     </p>
-                    <a href="https://drive.google.com/file/d/1cHOjCzyjgMyMLQQKqnFCQoPa-aqeDEWG/view?usp=drive_link" target="_blank"><button className="btn-rounded btn btn-outline-primary mt-4">Download CV</button></a>
+                    <a href="https://drive.google.com/file/d/1I6vBruiKbH_Bepr2sC5RNFz_mG7z2u8U/view?usp=sharing" target="_blank"><button className="btn-rounded btn btn-outline-primary mt-4">Download CV</button></a>
                 </div>
             </div>
         </div>
